7daa47bf46f1b4a6685860cb81ffbe2c9fd4de0b
yahya-bader-khawam <58319863+yahya-bader-khawam@users.noreply.github.com>
2023-07-02 22:05:23 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -23,5 +23,5 @@ setup(
     description=DESCRIPTION,
     long_description_content_type="text/markdown",
     packages=find_packages(),
-    install_requires=['numpy', 'pandas', 'scipy', 'torch', 'math', 'statsmodels']
+    install_requires=['numpy', 'pandas', 'scipy', 'torch', 'python-math', 'statsmodels']
 )
