0b8bad05c98b0731a6f7ed071d378ba5bb7451f0
yahya-bader-khawam <58319863+yahya-bader-khawam@users.noreply.github.com>
2023-07-02 22:27:31 -0400

    Update setup.py

--- setup.py
+++ setup.py
@@ -10,11 +10,6 @@ Original file is located at
 from setuptools import setup, find_packages
 import codecs
 import os
-
-here = os.path.abspath(os.path.dirname(__file__))
-
-with codecs.open(os.path.join(here, "README.md"), encoding="utf-8") as fh:
-    long_description = "\n" + fh.read()
     
 VERSION = '0.0.1'
 DESCRIPTION = 'SciModelStats class code for course use only'
